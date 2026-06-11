beebcd0f447cec2007129ba6dbd0f724ad05d10b
DansuraitoAhiru <vqviet2007@gmail.com>
2026-06-11 14:33:24 +0700

    Update test_ticketing.py

--- HNKS25CNTT4_Python_Session20_FinalProject/test_ticketing.py
+++ HNKS25CNTT4_Python_Session20_FinalProject/test_ticketing.py
@@ -1,21 +1,16 @@
-import unittest
+import pytest
 from main import calculate_total_revenue
 
 
-class TestRevenue(unittest.TestCase):
+def test_booked_and_cancelled():
+    ticket_list = [
+        {"price": 500, "status": "Booked"},
+        {"price": 300, "status": "Cancelled"},
+        {"price": 700, "status": "Booked"}
+    ]
 
-    def test_booked_and_cancelled(self):
-        ticket_list = [
-            {"price": 500, "status": "Booked"},
-            {"price": 300, "status": "Cancelled"},
-            {"price": 700, "status": "Booked"}
-        ]
-
-        self.assertEqual(calculate_total_revenue(ticket_list), 1200.0)
-
-    def test_empty_list(self):
-        self.assertEqual(calculate_total_revenue([]), 0.0)
+    assert calculate_total_revenue(ticket_list) == 1200.0
 
 
-if __name__ == "__main__":
-    unittest.main()
+def test_empty_list():
+    assert calculate_total_revenue([]) == 0.0
